b49a8c34749d7bee7d278fe9c7374cfb4c90f508
SteveC <cheema35@hotmail.com>
2020-01-31 11:25:21 -0800

    random ball script added

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -5,10 +5,25 @@ export default class Ball {
     this.radius = radius;
     this.boardWidth = boardWidth;
     this.boardHeight = boardHeight;
+    this.direction = 1;
+    this.reset();
+  }
+
+  reset() {
     this.x = this.boardWidth / 2;
     this.y = this.boardHeight / 2;
-    this.direction = 1;
+    this.vy = 0;
+    while (this.vy === 0) {
+      this.vy = Math.random() * 10 - 5;
+    }
+    this.vx = (6 - Math.abs(this.vy)) * this.direction;
   }
+
+  ballMove() {
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+  }
+
   render(svg) {
     const circleBall = document.createElementNS(SVG_NS, "circle");
     circleBall.setAttributeNS(null, "cx", this.x);
@@ -16,5 +31,6 @@ export default class Ball {
     circleBall.setAttributeNS(null, "r", this.radius);
     circleBall.setAttributeNS(null, "fill", "white");
     svg.appendChild(circleBall);
+    this.ballMove();
   }
 }
